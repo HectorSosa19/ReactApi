ddef1979fffb7babf365f1943ad95f4632e10cd9
hectorsosa19 <hectorsosa931@gmail.com>
2022-04-27 02:26:09 -0400

    React Asp Original

--- ReactPokemon/pokeapp/src/App.js
+++ ReactPokemon/pokeapp/src/App.js
@@ -1,64 +1,64 @@
-import React, { Component ,useState,useEffect} from "react";
+import React, {useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import axios from "axios";
-import {
-	Table,
-	Button,
-	Modal,
-	ModalBody,
-	ModalHeader,
-	FormGroup,
-	ModalFooter,
-	Container,
-} from "reactstrap";
-;
 
 
-function App(){
+function App() {
 	const baseUrl = "http://localhost:5000/api/Pokemon";
-	const [data, setData]=useState([]);
-	const [gestorSeleccionado, setGestorSeleccionado]=useState([]);
-	const handlechange=e=>{
-		const{name,value}=e.target;
-		setGestorSeleccionado({
-		  ...gestorSeleccionado,
-		  [name]:value
-		});
-		console.log(gestorSeleccionado);
-	  }
+	const [favorites, setFavorites] = useState([]);
+	const [data, setData] = useState([]);
 
-	const peticionGet=async()=>{
-		await axios.get(baseUrl)
-		.then(response=>{
-		  setData(response.data);
-		}).catch(error=>{
-		  console.log(error);
-		})
-	  }
-	  useEffect(()=>{
+	const peticionGet = async () => {
+		fetch(baseUrl)
+			.then((res) => res.json())
+			.then((result) => {
+				setData(result.results);
+			});
+	};
+
+	useEffect(() => {
 		peticionGet();
-	  },[])
-	
-    
-	return (
-		<div >
-		<table className="table table-bordered">
-          <thead>
-            <tr>
-              <th>name</th>
-            </tr>
-         </thead>
-         <tbody>
-        {data.map(gestor=>(
-          <tr key={gestor.name}>
-            <td>{gestor.name}</td>
-          </tr>
-		
-		  ))}
-		  </tbody>
-		  </table>
+		console.log(data);
+	}, []);
 
-	</div>
-		);
+	useEffect(() => {
+		console.log(favorites);
+	  }, [favorites]);
+	
+	function handleFavorite(name) {
+		const newFavorites = favorites.map(gestor => {
+		  return gestor.name === name ? { ...gestor, favorite: !gestor.favorite } : gestor;
+		});
+	
+		setFavorites(newFavorites);
+	  }
+	return (
+		<div>
+			<table className="table table-bordered">
+				<thead>
+					<tr>
+						<th>Name</th>
+					</tr>
+				</thead>
+				<tbody>
+					{data.map((gestor) => (
+						<tr key={gestor.name}>
+							<td>{gestor.name}</td>
+							<td>
+							<button onClick={()=>{handleFavorite(gestor.name)}}>
+								{gestor.name ===true? "❌":"❤"}
+							</button>
+							</td>
+						</tr>
+						))}
+				</tbody>
+			</table>
+			<h1>Favorite list</h1>
+      		<ul>
+        	{favorites.map(gestor =>
+          	gestor.favorite === false ? <li key={gestor.name}>{gestor.name}</li> : null
+        	)}
+      </ul>
+		</div>
+	);
 }
 export default App;
